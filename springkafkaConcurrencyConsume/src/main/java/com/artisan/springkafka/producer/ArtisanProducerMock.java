b4bf3522c495e8a8a2b93d7a113cf334e9cad049
yangshangwei <yswcomeon@gmail.com>
2021-02-18 20:54:33 +0800

    Apache Kafka-并发消费

---
+++ springkafkaConcurrencyConsume/src/main/java/com/artisan/springkafka/producer/ArtisanProducerMock.java
@@ -0,0 +1,47 @@
+package com.artisan.springkafka.producer;
+
+import com.artisan.springkafka.constants.TOPIC;
+import com.artisan.springkafka.domain.MessageMock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author 小工匠
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/2/17 22:25
+ * @mark: show me the code , change the world
+ */
+
+@Component
+public class ArtisanProducerMock {
+
+
+    @Autowired
+    private KafkaTemplate<Object,Object> kafkaTemplate ;
+
+
+    /**
+     * 同步发送
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    public SendResult sendMsgSync() throws ExecutionException, InterruptedException {
+        // 模拟发送的消息
+        Integer id = new Random().nextInt(100);
+        MessageMock messageMock = new MessageMock(id,"artisanTestMessage-" + id);
+        // 同步等待
+        return  kafkaTemplate.send(TOPIC.TOPIC, messageMock).get();
+    }
+
+
+
+}
+    
---
+++ springkafkaConcurrencyConsume/src/test/java/com/artisan/springkafka/produceTest/ProduceMockTest.java
@@ -0,0 +1,54 @@
+package com.artisan.springkafka.produceTest;
+
+import com.artisan.springkafka.SpringkafkaApplication;
+import com.artisan.springkafka.producer.ArtisanProducerMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author 小工匠
+ * * @version 1.0
+ * @description: TODO
+ * @date 2021/2/17 22:40
+ * @mark: show me the code , change the world
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringkafkaApplication.class)
+public class ProduceMockTest {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    @Autowired
+    private ArtisanProducerMock artisanProducerMock;
+
+
+    @Test
+    public void testAsynSend() throws ExecutionException, InterruptedException {
+        logger.info("开始发送");
+
+        for (int i = 0; i < 20; i++) {
+            artisanProducerMock.sendMsgSync();
+        }
+
+
+        // 阻塞等待，保证消费
+        new CountDownLatch(1).await();
+
+    }
+
+}
+    
