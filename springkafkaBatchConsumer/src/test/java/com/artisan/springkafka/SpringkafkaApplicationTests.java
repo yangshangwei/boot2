f85d96ae2f98b46b8173a2adc0bc98f1a88af64b
yangshangwei <yswcomeon@gmail.com>
2021-02-18 12:24:04 +0800

    Apache Kafka-批量消费消息

---
+++ springkafkaBatchConsumer/src/test/java/com/artisan/springkafka/SpringkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artisan.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
