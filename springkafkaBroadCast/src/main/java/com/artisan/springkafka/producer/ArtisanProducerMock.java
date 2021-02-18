6486eb00b1489fd9fa0f208b99ead1e0125d4d17
yangshangwei <yswcomeon@gmail.com>
2021-02-18 14:11:16 +0800

    Apache Kafka-广播消费

---
+++ springkafkaBroadCast/src/main/java/com/artisan/springkafka/producer/ArtisanProducerMock.java
@@ -0,0 +1,47 @@
+package com.artisan.springkafka.producer;
+
+import com.artisan.springkafka.constants.TOPIC;
+import com.artisan.springkafka.domain.MessageMock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @author 小工匠
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/2/17 22:25
+ * @mark: show me the code , change the world
+ */
+
+@Component
+public class ArtisanProducerMock {
+
+
+    @Autowired
+    private KafkaTemplate<Object,Object> kafkaTemplate ;
+
+
+    /**
+     * 异步发送
+     * @return
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    public ListenableFuture<SendResult<Object, Object>> sendMsgASync() throws ExecutionException, InterruptedException {
+        // 模拟发送的消息
+        Integer id = new Random().nextInt(100);
+        MessageMock messageMock = new MessageMock(id,"messageSendByAsync-" + id);
+        // 异步发送消息
+        ListenableFuture<SendResult<Object, Object>> result = kafkaTemplate.send(TOPIC.TOPIC, messageMock);
+        return result ;
+
+    }
+
+}
+    
